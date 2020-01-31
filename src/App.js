fb286bce8e2f967b74d4e78eff112cff0cb68ee8
Humayun Rashid <humayun@Humayuns-iMac.local>
2020-01-31 13:04:39 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Header from "./components/Header.js";
 import { Route } from "react-router-dom";
+import TabNav from "./components/TabNav.js";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import LocationList from "./components/LocationsList";
+import EpisodeList from "./components/EpisodeList";
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <TabNav />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/character" component={CharacterList} />
+      <Route path="/location" component={LocationList} />
+      <Route path="/episode" component={EpisodeList} />
     </main>
   );
 }
