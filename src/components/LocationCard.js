c052bde66e801d9609cdf0d6290a063512b2982a
Humayun Rashid <humayun@Humayuns-iMac.local>
2020-01-31 12:36:39 -0500

    LocationCard

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+// export default function LocationCard({ name, type, dimension, residents }) {
+//   return <span>todo: location</span>;
+// }
+
+export default function LocationCard(props) {
+  return (
+    <Card key={props.id}>
+      <Card.Content>
+        <Card.Header>{props.name}</Card.Header>
+        <Card.Description>
+          {`${props.type} - ${props.dimension}`}
+        </Card.Description>
+        <Card.Meta>{props.residents}</Card.Meta>
+      </Card.Content>
+    </Card>
+
+  )
 }
